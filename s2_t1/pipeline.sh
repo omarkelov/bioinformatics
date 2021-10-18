2c3e6ed65025d30ceeea487ed22ed271f5ddf510
Oleg Markelov <o.markelov@g.nsu.ru>
2021-10-19 00:25:21 +0700

    Add task 2 part 1

---
+++ s2_t1/pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+fastqc $1.fastq
+mv $1_fastqc.html sra_data.html
+rm $1_fastqc.zip
+minimap2 -d ref.mmi ref.fna
+minimap2 -a ref.mmi $1.fastq > $1.sam
+samtools flagstat $1.sam > $1_flagstat.txt
+
+percent=$(grep -oP '\d+\.\d+(?=%)' $1_flagstat.txt | head -1 | tr -d .)
+if ((10#$percent > 9000)); then
+   echo "OK"
+else
+   echo "not OK"
+fi
