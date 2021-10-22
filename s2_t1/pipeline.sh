f1b2e7a9491c4df6216b39c42a3d49f370068e54
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-10-22 12:21:40 +0700

    Test

--- s2_t1/pipeline.sh
+++ s2_t1/pipeline.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 fastqc $1.fastq
-mv $1_fastqc.html sra_data.html
+mv $1_fastqc.html $1.html
 rm $1_fastqc.zip
 minimap2 -d ref.mmi ref.fna
 minimap2 -a ref.mmi $1.fastq > $1.sam
 samtools flagstat $1.sam > $1_flagstat.txt
 
-percent=$(grep -oP '\d+\.\d+(?=%)' $1_flagstat.txt | head -1 | tr -d .)
+percent=$(grep -oP '\d+\.\d+(?=%)' $1_flagstat.txt | head -1 | tr -d .) # todo regex
 if ((10#$percent > 9000)); then
    echo "OK"
 else
